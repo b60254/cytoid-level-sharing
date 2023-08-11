e8bb9274262044fdd42613d2fc345bf03f688035
b60254 <50722804+b60254@users.noreply.github.com>
2023-08-11 04:08:33 -1000

    Add localized artist and sources, if available
    Co-Authored-By: Karson <81365289+ks62-hk@users.noreply.github.com>

--- chart_sharing.py
+++ chart_sharing.py
@@ -1,35 +1,62 @@
 # pasta's script to automate getting level data for chart sharing
 import json
+from urllib.parse import urlparse
 
 with open('level.json', 'r', encoding = 'utf-8') as f:
     level = json.load(f)
 
-def list_difficulties():
+def is_valid_url(str):
+    parsed = urlparse(str)
+    return (parsed.scheme and parsed.netloc)
+def print_title():
+    if ('title_localized' in level and not (level['title_localized'] is None)):
+        print(f'Title: {title}（{level["title_localized"]}）')
+    else:
+        print(f'Title: {title}')
+
+def print_difficulties():
     difficulty_list = []
 
     for chart in level['charts']:
         difficulty_list.append(chart['difficulty'])
     
     difficulty_list.sort()
-    return difficulty_list
+    if (len(difficulty_list) == 1):
+        print('Difficulty: ', end = '')
+    else:
+        print('Difficulties: ', end = '')
+    print(*difficulty_list, sep = ', ')
+
+def print_artist():
+    global artist
+    if ('artist_localized' in level and not (level['artist_localized'] is None)):
+        artist += '（' + level['artist_localized'] + '）'
+    if (is_valid_url(level['artist_source'])):
+        print(f'Artist: [{artist}](<{artist_source}>)')
+    else:
+        print(f'Artist: {level["artist"]}')
+
+def print_illustrator():
+    if (is_valid_url(level['illustrator_source'])):
+        print(f'Illustrator: [{illustrator}](<{illustrator_source}>)')
+    else:
+        print(f'Illustrator: {level["illustrator"]}')
 
 charter = level['charter']
 artist = level['artist']
 title = level['title']
-difficulties = list_difficulties()
+artist_source = level['artist_source']
+illustrator = level['illustrator']
+illustrator_source = level['illustrator_source']
 
 
 print('Post title:')
 print(f"[{charter}] {artist} - {title}\n")
 
 print('Description:')
-print(f"""Title: {title}
-Level ID: {level['id']}
-Artist: {artist}
-Charter: {charter}""")
-if (len(difficulties) == 1):
-    print('Difficulty: ', end = '')
-else:
-    print('Difficulties: ', end = '')
-print(*difficulties, sep = ', ')
-print(f'Illustrator: {level["illustrator"]}')
+print_title()
+print(f"Level ID: {level['id']}")
+print_artist()
+print(f'Charter: {charter}')
+print_difficulties()
+print_illustrator()
